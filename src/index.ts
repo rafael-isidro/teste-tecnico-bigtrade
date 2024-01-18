ac835467b6f140b1885579a908f4ee06e7681f30
rafael-isidro <rafa-isidro@hotmail.com>
2024-01-18 00:10:22 -0300

    Fix: removed type module from package.json and API test at https://localhost:PORT

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from 'express';
+import express from "express";
+import "dotenv/config";
 
 const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  return res.send("API working");
+});
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on PORT: ${PORT}`)
+);
 
 export default server;
